f29e59384277124dd0e189b0829e86c67f00a603
Caranatar <caranatar@riseup.net>
2020-07-04 18:57:48 -0400

    Updates

---
+++ precache-manifest.25926195e10e0c916d53db4b5ea2dc9f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8d9d59230240dc54b81f8f16c322bd14",
+    "url": "/tracery-builder/index.html"
+  },
+  {
+    "revision": "b9dfa07e56d4ca215b1b",
+    "url": "/tracery-builder/static/css/main.a065e277.chunk.css"
+  },
+  {
+    "revision": "a5ffd637715eb4bea8b5",
+    "url": "/tracery-builder/static/js/2.34446ec9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/tracery-builder/static/js/2.34446ec9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b9dfa07e56d4ca215b1b",
+    "url": "/tracery-builder/static/js/main.e68842e7.chunk.js"
+  },
+  {
+    "revision": "31d2baccfa219b297f52",
+    "url": "/tracery-builder/static/js/runtime-main.725f03f1.js"
+  },
+  {
+    "revision": "9329fad2b936fccc55bc18bca673a642",
+    "url": "/tracery-builder/static/media/jazz.9329fad2.png"
+  }
+]);
